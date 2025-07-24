1adee90cc000536ee2ae79438e3699fc610c709e
VitaliiKononenkoMM <vitaliy.kononenko@mealticket.com>
2025-07-24 09:44:29 +0200

    Initial Commit

---
+++ team_managerTests/team_managerTests.swift
@@ -0,0 +1,17 @@
+//
+//  team_managerTests.swift
+//  team_managerTests
+//
+//  Created by Vitaliy Kononenko on 24/07/2025.
+//
+
+import Testing
+@testable import team_manager
+
+struct team_managerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
